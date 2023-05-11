6305735ecbe131ecc41ed4132fc58a9fc9e75055
Artem <kolyasik124@ukr.net>
2023-05-11 16:01:55 +0300

    first commit

---
+++ src/components/Card.tsx
@@ -0,0 +1,27 @@
+import styles from './card.module.css';
+
+type film = {
+    name: string;
+    img: string;
+    genre: string;
+    releaseDate: number;
+    country: string;
+    rating: number;
+};
+
+type CardProps = {
+    film: film;
+};
+
+export function Card({ film }: CardProps) {
+    return (
+        <div className={styles.card}>
+            <h3>{film.name}</h3>
+            <img src={film.img} />
+            <p>Жанр: {film.genre}</p>
+            <p>Дата виходу: {film.releaseDate}</p>
+            <p>Країна: {film.country}</p>
+            <p>Рейтинг: {film.rating}/10</p>
+        </div>
+    );
+}
---
+++ src/pages/info.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+
+const Info: NextPage = () => {
+  return (
+    <div>Some info</div>
+  )
+}
+
+export default Info;
