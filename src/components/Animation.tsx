797c707b8ca02af034f1aeebd869c946fcbc8611
Artem <kolyasik124@ukr.net>
2023-05-24 23:39:03 +0300

    animation background +-

---
+++ src/components/Animation.tsx
@@ -0,0 +1,13 @@
+import styles from './animation.module.css';
+
+export function Animation() {
+    return (
+        <div className={styles.animation_background}>
+            <div className={styles.animation_projector}></div>
+            <div className={styles.animation_check}></div>
+            <div className={styles.animation_}></div>
+            <div className={styles.animation_}></div>
+            <div className={styles.animation_}></div>
+        </div>
+    );
+}
