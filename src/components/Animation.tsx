b0979ba66dd47fc37da1abbc83537b2d86b89d0f
Artem <kolyasik124@ukr.net>
2023-05-25 00:02:13 +0300

    animation +-

--- src/components/Animation.tsx
+++ src/components/Animation.tsx
@@ -5,7 +5,7 @@ export function Animation() {
         <div className={styles.animation_background}>
             <div className={styles.animation_projector}></div>
             <div className={styles.animation_check}></div>
-            <div className={styles.animation_}></div>
+            <div className={styles.animation_open}></div>
             <div className={styles.animation_}></div>
             <div className={styles.animation_}></div>
         </div>
