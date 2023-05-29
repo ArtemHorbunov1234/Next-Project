5a9d5d7f2d7272cfe49a03e968f817584825ef5c
Artem <kolyasik124@ukr.net>
2023-05-29 23:26:00 +0300

    animation+

--- src/components/Animation.tsx
+++ src/components/Animation.tsx
@@ -3,11 +3,12 @@ import styles from './animation.module.css';
 export function Animation() {
     return (
         <div className={styles.animation_background}>
-            <div className={styles.animation_projector}></div>
+            <div className={styles.animation_reel}></div>
             <div className={styles.animation_check}></div>
             <div className={styles.animation_open}></div>
-            <div className={styles.animation_}></div>
-            <div className={styles.animation_}></div>
+            <div className={styles.animation_settings}></div>
+            <div className={styles.animation_projector}></div>
+            <div className={styles.animation_regular}></div>
         </div>
     );
 }
